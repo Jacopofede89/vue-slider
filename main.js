b1fc7792ef2a0e10d90ed6b927750adc149f4523
Jacopofede89 <jacopofede89@gmail.com>
2021-11-11 16:13:13 +0100

    sviluppo

---
+++ main.js
@@ -0,0 +1,53 @@
+var carusel = new Vue({
+    el:"#app",
+    data:{
+        activeSlide: 0,
+        slides: [
+            {
+                image: "img/01.jpg",
+                title: "Svezia",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+            },
+            {
+                image: "img/02.jpg",
+                title: "Svizzera",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+            },
+            {
+                image: "img/03.jpg",
+                title: "Gran Bretagna",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+            },
+            {
+                image: "img/04.jpg",
+                title: "Germania",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+            },
+            {
+                image: "img/05.jpg",
+                title: "Paradise",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+            },
+        ]
+    },
+
+    methods: {
+        nextSlide: function(){
+            if(this.activeSlide === this.slides.length -1){
+                this.activeSlide = 0;
+            } else {
+            this.activeSlide ++;
+            }
+           
+        },
+
+        prevSlide: function(){
+            if(this.activeSlide === 0){
+                this.activeSlide = this.slides.length -1;
+            } else {
+            this.activeSlide --;
+            }
+           
+        }
+    }
+});
